ba4a34493d599e5ec772f4c499d6feab6ba70e00
danie odukwe <kingdanny295@gmail.com>
2024-01-24 14:38:44 +0100

    added context type in type index

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,24 @@
-import React from 'react'
+import { createContext, useContext, useEffect, useState } from "react"
+
+export const INITIAL_USER = {
+    id: '',
+    name: '',
+    username: '',
+    email: '',
+    imageUrl: '',
+    bio: '',
+};
+
+const INITIAL_STATE = {
+    user: INITIAL_USER,
+    isLoading: false,
+    isAuthenticated: false,
+    setUser: () => {},
+    setIsAuthenticated: () => {},
+    checkAuthUser: async ()=> false as boolean,
+}
+
+const AuthContext = createContext<>
 
 const AuthContext = () => {
   return (
